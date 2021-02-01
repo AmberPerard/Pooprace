1f8d8b7cf2248bf1047f03a31622b67cf2adc24d
Moongirl1207 <amber.perard@student.howest.be>
2021-02-01 13:01:51 +0100

    added screens on right moment and has ranking in text

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,43 +1,76 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class GameController : MonoBehaviour
 {
     public List<MoveCar> Carlist = new List<MoveCar>(4);
-    public List<GameObject> playerPos = new List<GameObject>(4);
+
     public Transform target1;
     public Transform target2;
     public Transform target3;
     public Transform target4;
 
+    public Text Place1;
+    public Text Place2;
+    public Text Place3;
+    public Text Place4;
 
-    private Vector3 firstP = new Vector3(-34f, 2.4f, 0.8f);
-    private Vector3 secondP = new Vector3(-3.4f,1.7f, 0.8f);
-    private Vector3 thirdP = new Vector3(-3.4f, 1.3f, 0.8f);
-    private Vector3 forthP = new Vector3(-3.4f, 0.9f, 0.8f);
+    // The popscreens for the info
+    public GameObject beginScreen;
+    public GameObject endScreen;
+
+    public float startTimer = 10f;
+    public Text StartTimer;
+
+    public bool gameIsActive = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // start with a clean score
+        PlayerPrefs.DeleteAll();
+
+        // start the game timer
+        beginScreen.SetActive(true);
+        endScreen.SetActive(false);
+        gameIsActive = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // sort cars 
-        Carlist.Sort((a, b) => (a.position).CompareTo(b.position));
-        Carlist.Reverse();
+        startTimer -= Time.deltaTime;
+        int seconds = Mathf.FloorToInt(startTimer);
+        StartTimer.text = seconds.ToString();
+
+        if (startTimer <= 0)
+        {
+            beginScreen.SetActive(false);
+            gameIsActive = true;
+            // sort cars 
+            Carlist.Sort((a, b) => (a.position).CompareTo(b.position));
+            Carlist.Reverse();
+
+            Carlist[0].scoreboardPosition.transform.position = Vector3.MoveTowards(transform.position, target1.position, 1000);
+            Carlist[1].scoreboardPosition.transform.position = Vector3.MoveTowards(transform.position, target2.position, 1000);
+            Carlist[2].scoreboardPosition.transform.position = Vector3.MoveTowards(transform.position, target3.position, 1000);
+            Carlist[3].scoreboardPosition.transform.position = Vector3.MoveTowards(transform.position, target4.position, 1000);
+
+            Debug.Log(Carlist[0].carName);
+        }
 
 
-        //Debug.Log(Carlist[0].scoreboardPosition.transform.position);
-        Carlist[0].scoreboardPosition.transform.position = Vector3.MoveTowards(transform.position, target1.position, 1000);
-        Carlist[1].scoreboardPosition.transform.position = Vector3.MoveTowards(transform.position, target2.position, 1000);
-        Carlist[2].scoreboardPosition.transform.position = Vector3.MoveTowards(transform.position, target3.position, 1000);
-        Carlist[3].scoreboardPosition.transform.position = Vector3.MoveTowards(transform.position, target4.position, 1000);
-
-        Debug.Log(Carlist[0].carName);
-
-
+        if (Carlist[0].end == true)
+        {
+            endScreen.SetActive(true);
+            gameIsActive = false;
+            Place1.text = Carlist[0].scoreboardPosition.name;
+            Place2.text = Carlist[1].scoreboardPosition.name;
+            Place3.text = Carlist[2].scoreboardPosition.name;
+            Place4.text = Carlist[3].scoreboardPosition.name;
+        }
     }
 }
--- Assets/Scripts/MoveCar.cs
+++ Assets/Scripts/MoveCar.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class MoveCar : MonoBehaviour
 {
+    public GameController gameController;
+
     public Transform target;
     public float step = 0.0001f;
     public string keyInput;
@@ -12,28 +14,33 @@ public class MoveCar : MonoBehaviour
     public GameObject scoreboardPosition;
     public int position = 0;
 
+    public bool end = false; 
+
     // Start is called before the first frame update
     void Start()
     {
         carName = gameObject.name;
+        end = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-//Debug.Log( carName + position);
-
-        if (Input.GetKeyDown(keyInput))
+        //Debug.Log( carName + position);
+        if (gameController.gameIsActive == true)
         {
-            transform.position = Vector3.MoveTowards(transform.position, target.position, step / 4);
-            position = position +  1;
-        }
+            if (Input.GetKeyDown(keyInput))
+            {
+                transform.position = Vector3.MoveTowards(transform.position, target.position, step / 4);
+                position = position + 1;
+            }
 
-        if (transform.position == target.position)
-        {
-            Debug.Log("This car " + gameObject.name.ToString() + " has hit the finish line");
+            if (transform.position == target.position)
+            {
+                end = true;
+                Debug.Log("This car " + gameObject.name.ToString() + " has hit the finish line");
+            }
         }
      }
-
 
 }
