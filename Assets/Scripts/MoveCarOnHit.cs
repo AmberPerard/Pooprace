b9135131943b2eebd8d97313e7f50419a470fabe
ellen sierens <ellen.sierens@student.howest.be>
2021-02-04 18:32:38 +0100

    add arduino sketch

--- Assets/Scripts/MoveCarOnHit.cs
+++ Assets/Scripts/MoveCarOnHit.cs
@@ -10,19 +10,19 @@ public class MoveCarOnHit : MonoBehaviour
     {
         GameObject currentObject1 = GameObject.Find("Player1Car");
         MoveCar currentObjectMove1 = currentObject1.GetComponent<MoveCar>();
-        currentObjectMove1.keyInput = "q";
+        currentObjectMove1.keyInput = "f";
 
         GameObject currentObject2 = GameObject.Find("Player2Car");
         MoveCar currentObjectMove2 = currentObject2.GetComponent<MoveCar>();
-        currentObjectMove2.keyInput = "s";
+        currentObjectMove2.keyInput = "g";
 
         GameObject currentObject3 = GameObject.Find("Player3Car");
         MoveCar currentObjectMove3 = currentObject3.GetComponent<MoveCar>();
-        currentObjectMove3.keyInput = "l";
+        currentObjectMove3.keyInput = "h";
 
         GameObject currentObject4 = GameObject.Find("Player4Car");
         MoveCar currentObjectMove4 = currentObject4.GetComponent<MoveCar>();
-        currentObjectMove4.keyInput = "m";
+        currentObjectMove4.keyInput = "j";
 
     }
 
